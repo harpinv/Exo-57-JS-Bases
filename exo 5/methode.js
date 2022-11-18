c1179a8d2402f34dad2d9e6d403242063ab25e21
harpin <vincent.harpin@orange.fr>
2022-11-18 20:05:26 +0100

    exercice 57

---
+++ exo 5/methode.js
@@ -0,0 +1,5 @@
+let myVirgule = prompt('entrer un nombre à virgule');
+let myPoint = prompt('entrer un nombre à virgule');
+let result = parseFloat(myVirgule) * parseFloat(myPoint);
+
+alert(result.toString());
---
+++ exo 6/methode.js
@@ -0,0 +1,8 @@
+let myAge = prompt('entrer votre âge');
+let myNum = parseFloat(myAge);
+
+if (myNum >= 18) {
+    alert('Vous êtes majeur');
+} else {
+    alert('Vous êtes mineur');
+}
